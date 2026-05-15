c476a9f562c8a28d0a5e12291dafc3d31389cf12
jemwy <jeremylucero@cpp.edu>
2026-05-15 15:20:21 -0700

    Adding Homework10 Challenges

---
+++ Homework10/Chapter20/src/com/craftinginterpreters/lox/table.h
@@ -0,0 +1,28 @@
+#ifndef clox_table_h
+#define clox_table_h
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+  Value key;
+  Value value;
+} Entry;
+
+typedef struct {
+  int count;
+  int capacity;
+  Entry* entries;
+} Table;
+
+ObjString* tableFindString(Table* table, const char* chars,
+                           int length, uint32_t hash);
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableGet(Table* table, Value key, Value* value);
+bool tableSet(Table* table, Value key, Value value);
+bool tableDelete(Table* table, Value key);
+void tableAddAll(Table* from, Table* to);
+
+#endif
---
+++ Homework10/Chapter20/src/com/craftinginterpreters/lox/value.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "object.h"
+#include "memory.h"
+#include "value.h"
+
+void initValueArray(ValueArray* array) {
+  array->values = NULL;
+  array->capacity = 0;
+  array->count = 0;
+}
+
+void writeValueArray(ValueArray* array, Value value) {
+  if (array->capacity < array->count + 1) {
+    int oldCapacity = array->capacity;
+    array->capacity = GROW_CAPACITY(oldCapacity);
+    array->values = GROW_ARRAY(Value, array->values,
+                               oldCapacity, array->capacity);
+  }
+
+  array->values[array->count] = value;
+  array->count++;
+}
+
+void freeValueArray(ValueArray* array) {
+  FREE_ARRAY(Value, array->values, array->capacity);
+  initValueArray(array);
+}
+
+void printValue(Value value) {
+  switch (value.type) {
+    case VAL_BOOL:
+      printf(AS_BOOL(value) ? "true" : "false");
+      break;
+    case VAL_NIL: printf("nil"); break;
+    case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+    case VAL_OBJ: printObject(value); break;
+  }
+}
+
+bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type) return false;
+  switch (a.type) {
+    case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
+    case VAL_NIL:    return true;
+    case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+    case VAL_OBJ:    return AS_OBJ(a) == AS_OBJ(b);
+    default:         return false; // Unreachable.
+  }
+}
+
+static uint32_t hashNumber(double number) {
+  uint64_t bits;
+  memcpy(&bits, &number, sizeof(double));
+  return (uint32_t)(bits ^ (bits >> 32));
+}
+
+uint32_t hashValue(Value value) {
+  switch (value.type) {
+    case VAL_BOOL:
+      return AS_BOOL(value) ? 3 : 5;
+
+    case VAL_NIL:
+      return 7;
+
+    case VAL_NUMBER:
+      return hashNumber(AS_NUMBER(value));
+
+    case VAL_OBJ:
+      if (IS_STRING(value)) {
+        return AS_STRING(value)->hash;
+      }
+
+      return (uint32_t)(uintptr_t)AS_OBJ(value);
+  }
+
+  return 0;
+}
